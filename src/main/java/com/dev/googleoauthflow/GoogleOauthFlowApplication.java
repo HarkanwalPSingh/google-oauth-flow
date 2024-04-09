6a496625c97ce6ad1ccf4a3a12b9bbf7640d5108
Kanwal19 <hkps17@gmail.com>
2024-04-10 02:23:38 +0530

    Initial Commit

---
+++ src/main/java/com/dev/googleoauthflow/GoogleOauthFlowApplication.java
@@ -0,0 +1,13 @@
+package com.dev.googleoauthflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleOauthFlowApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GoogleOauthFlowApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/dev/googleoauthflow/GoogleOauthFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.googleoauthflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleOauthFlowApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
